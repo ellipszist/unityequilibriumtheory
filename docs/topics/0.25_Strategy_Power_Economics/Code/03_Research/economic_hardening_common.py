51077bd7c13526c5f51df14667a8c53ec70c64a6
santxkoz <santazazagamer@gmail.com>
2026-07-16 12:51:22 +0700

    Integrate global strata robustness artifact

--- docs/topics/0.25_Strategy_Power_Economics/Code/03_Research/economic_hardening_common.py
+++ docs/topics/0.25_Strategy_Power_Economics/Code/03_Research/economic_hardening_common.py
@@ -58,6 +58,7 @@ GLOBAL_REPLICATION_ARTIFACT = ARTIFACT_DIR / "0_25_global_replication_readiness.
 GLOBAL_WDI_PANEL_ARTIFACT = ARTIFACT_DIR / "0_25_global_wdi_panel.json"
 GLOBAL_WDI_LOO_ARTIFACT = ARTIFACT_DIR / "0_25_global_wdi_leave_one_out.json"
 GLOBAL_WDI_PPP_ARTIFACT = ARTIFACT_DIR / "0_25_global_wdi_ppp_comparison.json"
+GLOBAL_WDI_STRATA_ARTIFACT = ARTIFACT_DIR / "0_25_global_wdi_strata_audit.json"
 
 
 def utc_now() -> str:
